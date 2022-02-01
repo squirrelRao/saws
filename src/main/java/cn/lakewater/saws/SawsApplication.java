7b7a582206b9db1efa6884a83b645a21af10fa21
squirrelRao <hqraop@163.com>
2022-02-01 15:50:50 +0800

    first commit project

---
+++ src/main/java/cn/lakewater/saws/SawsApplication.java
@@ -0,0 +1,13 @@
+package cn.lakewater.saws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SawsApplication.class, args);
+    }
+
+}
