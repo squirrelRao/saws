dbae040b02a8fff1f5b666faa12f7d705ba54d8d
squirrelRao <hqraop@163.com>
2022-02-02 14:35:03 +0800

    add main  controller

--- src/main/java/cn/lakewater/saws/SawsApplication.java
+++ src/main/java/cn/lakewater/saws/SawsApplication.java
@@ -2,12 +2,13 @@ package cn.lakewater.saws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class SawsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SawsApplication.class, args);
     }
-
 }
